64d68e08a7ded86c7ef7964ebecd7752df3cc7e9
ShunjiaLiu <142769361+ShunjiaLiu@users.noreply.github.com>
2023-12-13 18:50:02 -0600

    hw 14

--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,123 @@
-//Use the D3 library to read in samples.json
+// Use the D3 library to read in samples.json
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+function getPlots(id) {
+    // Read samples.json
+    d3.json(url).then(sampledata => {
+        
+        console.log(sampledata);
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-  console.log(data);
-});
+        // Extract and log otu_ids, sample_values, and otu_labels for the first sample
+        let ids = sampledata.samples[0].otu_ids;
+        console.log("otu_ids:", ids);
 
-//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-var dropdownMeun = d3.select("#selDataset");
-data.names.forEach(function(sample){
-  dropdownMenu.append("option").text(sample).property("value", sample);
-});
+        let sampleValues = sampledata.samples[0].sample_values.slice(0, 10).reverse();
+        console.log("sample_values:", sampleValues);
 
+        let labels = sampledata.samples[0].otu_labels.slice(0, 10);
+        console.log("otu_labels:", labels);
 
-//Use sample_values as the values for the bar chart.
-function updateBarchart(sample){
-  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-  .then(function(data){
-    var selectedSampleData = data.samples.filter(function(d){
-      return d.id === sample;
-    })[0];
+        // create bar chart
+        let OTU_top = sampledata.samples[0].otu_ids.slice(0, 10).reverse();
+        let OTU_id = OTU_top.map(d => "OTU " + d);
+        console.log("OTU IDS:", OTU_id);
 
-//Use otu_ids as the labels for the bar chart.
+        let trace = {
+            x: sampleValues,
+            y: OTU_id,
+            text: labels,
+            marker: {
+                color: 'blue'
+            },
+            type: "bar",
+            orientation: "h",
+        };
 
+        let data = [trace];
 
-//Use otu_labels as the hovertext for the chart.
+        let layout = {
+            title: "Top 10 OTU",
+            yaxis: {
+                tickmode: "linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 30
+            }
+        };
+
+        // Create bar plot
+        Plotly.newPlot("bar", data, layout);
+
+        // Process data to create bubble chart
+        let trace1 = {
+          x: sampledata.samples[0].otu_ids,
+          y: sampledata.samples[0].sample_values,
+          mode: "markers",
+          marker: {
+              size: sampledata.samples[0].sample_values,
+              color: sampledata.samples[0].otu_ids
+          },
+          text: sampledata.samples[0].otu_labels
+      };
+
+      let layout_2 = {
+          xaxis: { title: "OTU ID" },
+          height: 600,
+          width: 1000
+      };
+
+      let data1 = [trace1];
+
+      // Create bubble plot
+      Plotly.newPlot("bubble", data1, layout_2);
+  });
+}
+
+// Function to get demographic information
+function getDemoInfo(id) {
+  d3.json(url).then(data => {
+      let metadata = data.metadata;
+      console.log(metadata);
+
+      let result = metadata.filter(meta => meta.id.toString() === id)[0];
+      let demographicInfo = d3.select("#sample-metadata");
+
+      demographicInfo.html("");
+
+      Object.entries(result).forEach((key) => {
+          demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+      });
+  });
+}
+
+// Function for the change event
+function optionChanged(id) {
+  getPlots(id);
+  getDemoInfo(id);
+}
+
+// Function for the initial data rendering
+function init() {
+  // Select dropdown menu
+  let dropdown = d3.select("#selDataset");
+
+  // Read the data
+  d3.json(url).then(data => {
+      console.log(data);
+
+      // Get the id data to the dropdown menu
+      data.names.forEach(function (name) {
+          dropdown.append("option").text(name).property("value");
+      });
+
+      // display the data and the plots to the page
+      getPlots(data.names[0]);
+      getDemoInfo(data.names[0]);
+  });
+}
+
+// Call init function to initialize the page
+init();
